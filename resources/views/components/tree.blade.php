6882a1eb64ccf53f9bd3d939d0c88db1483063fa
HyanCat <hyancat@live.cn>
2015-05-19 20:56:20 +0800

    better tree view.

--- resources/views/components/tree.blade.php
+++ resources/views/components/tree.blade.php
@@ -7,23 +7,25 @@
  * Copyright (C) 2015 HyanCat. All rights reserved.
  */
 ?>
-<div class="ui wide sidebar accordion list segment">
-	@foreach($categories as $cate)
-		<div class="item">
-			<div class="active title">
-				<i class="dropdown icon"></i>
-				{{ $cate->name }}
-			</div>
-			<div class="active content">
-				<div class="ui animated selection list">
-					@foreach($cate->docs as $doc)
-						<a href="{{ route('doc.show', ['id' => $doc->id]) }}" class="item {{ isset($document) && $document->id == $doc->id ? 'active' : '' }}">
-							<i class="file outline icon"></i>
-							<div class="content"> {{ $doc->title }} </div>
-						</a>
-					@endforeach
+<div class="ui wide sidebar segment">
+	<div class="ui accordion list">
+		@foreach($categories as $cate)
+			<div class="item">
+				<div class="active title">
+					<i class="dropdown icon"></i>
+					{{ $cate->name }}
+				</div>
+				<div class="active content">
+					<div class="ui animated selection list">
+						@foreach($cate->docs as $doc)
+							<a href="{{ route('doc.show', ['id' => $doc->id]) }}" class="item {{ isset($document) && $document->id == $doc->id ? 'active' : '' }}">
+								<i class="file outline icon"></i>
+								<div class="content"> {{ $doc->title }} </div>
+							</a>
+						@endforeach
+					</div>
 				</div>
 			</div>
-		</div>
-	@endforeach
+		@endforeach
+	</div>
 </div>
